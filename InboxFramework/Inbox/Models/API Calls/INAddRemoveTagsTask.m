2c1b92538f52d3e44279712315576f52f913ac59
anthonyliao <alisforanthonyliao@gmail.com>
2014-09-23 16:55:08 -0400

    Add dependency of INAddRemoveTagsTask to INSaveTagTask
    Add dependency of INAddRemoveTagsTask to INSaveTagTask

--- InboxFramework/Inbox/Models/API Calls/INAddRemoveTagsTask.m
+++ InboxFramework/Inbox/Models/API Calls/INAddRemoveTagsTask.m
@@ -6,9 +6,11 @@
 //  Copyright (c) 2014 Inbox. All rights reserved.
 //
 
+#import "INSaveTagTask.h"
 #import "INAddRemoveTagsTask.h"
 #import "INThread.h"
 #import "INMessage.h"
+#import "INTag.h"
 
 @implementation INAddRemoveTagsTask
 
@@ -27,6 +29,32 @@
     return self.data[@"tagIDsToRemove"];
 }
 
+- (NSMutableArray *)tagIDStringsToAdd
+{
+    NSMutableArray * tagIDStrings = [NSMutableArray array];
+    for (NSObject * tag in [self tagIDsToAdd]) {
+        if ([tag isKindOfClass: [INTag class]]) {
+            [tagIDStrings addObject: [(INTag*)tag ID]];
+        } else {
+            [tagIDStrings addObject: (NSString*)tag];
+        }
+    }
+    return tagIDStrings;
+}
+
+- (NSMutableArray *)tagIDStringsToRemove
+{
+    NSMutableArray * tagIDStrings = [NSMutableArray array];
+    for (NSObject * tag in [self tagIDsToRemove]) {
+        if ([tag isKindOfClass: [INTag class]]) {
+            [tagIDStrings addObject: [(INTag*)tag ID]];
+        } else {
+            [tagIDStrings addObject: (NSString*)tag];
+        }
+    }
+    return tagIDStrings;
+}
+
 - (INThread*)thread
 {
     if ([self.model isKindOfClass: [INThread class]])
@@ -39,24 +67,24 @@
 - (NSURLRequest *)buildAPIRequest
 {
     NSAssert([self thread], @"INSaveDraftChange asked to buildRequest with no access to a thread model!");
-	NSAssert([[self thread] namespaceID], @"INSaveDraftChange asked to buildRequest with no namespace!");
-
+    NSAssert([[self thread] namespaceID], @"INSaveDraftChange asked to buildRequest with no namespace!");
+    
     NSError * error = nil;
     NSString * path = [[self thread] resourceAPIPath];
     NSString * url = [[NSURL URLWithString:path relativeToURL:[INAPIManager shared].AF.baseURL] absoluteString];
     
     NSMutableDictionary * params = [NSMutableDictionary dictionary];
-    [params setObject:[self tagIDsToAdd] forKey:@"add_tags"];
-    [params setObject:[self tagIDsToRemove] forKey:@"remove_tags"];
+    [params setObject:[self tagIDStringsToAdd] forKey:@"add_tags"];
+    [params setObject:[self tagIDStringsToRemove] forKey:@"remove_tags"];
     
-	return [[[[INAPIManager shared] AF] requestSerializer] requestWithMethod:@"PUT" URLString:url parameters:params error:&error];
+    return [[[[INAPIManager shared] AF] requestSerializer] requestWithMethod:@"PUT" URLString:url parameters:params error:&error];
 }
 
 - (void)applyLocally
 {
     NSMutableArray * newTagIDs = [NSMutableArray arrayWithArray: [[self thread] tagIDs]];
-    [newTagIDs addObjectsFromArray: self.tagIDsToAdd];
-    [newTagIDs removeObjectsInArray: self.tagIDsToRemove];
+    [newTagIDs addObjectsFromArray: self.tagIDStringsToAdd];
+    [newTagIDs removeObjectsInArray: self.tagIDStringsToRemove];
     [[self thread] setTagIDs: newTagIDs];
     [[INDatabaseManager shared] persistModel: [self thread]];
 }
@@ -64,8 +92,8 @@
 - (void)rollbackLocally
 {
     NSMutableArray * newTagIDs = [NSMutableArray arrayWithArray: [[self thread] tagIDs]];
-    [newTagIDs removeObjectsInArray: self.tagIDsToAdd];
-    [newTagIDs addObjectsFromArray: self.tagIDsToRemove];
+    [newTagIDs removeObjectsInArray: self.tagIDStringsToAdd];
+    [newTagIDs addObjectsFromArray: self.tagIDStringsToRemove];
     [[self thread] setTagIDs: newTagIDs];
     [[INDatabaseManager shared] persistModel: [self thread]];
 }
@@ -84,15 +112,29 @@
         INAddRemoveTagsTask * otherTask = (INAddRemoveTagsTask*)other;
         
         // do we add ALL the tags 'other' is removing?
-        BOOL invalidatesRemoved = [[NSSet setWithArray: [otherTask tagIDsToRemove]] isSubsetOfSet:[NSSet setWithArray: [self tagIDsToAdd]]];
-
+        BOOL invalidatesRemoved = [[NSSet setWithArray: [otherTask tagIDStringsToRemove]] isSubsetOfSet:[NSSet setWithArray: [self tagIDStringsToAdd]]];
+        
         // do we remove ALL the tags 'other' is adding?
-        BOOL invalidatesAdded = [[NSSet setWithArray: [otherTask tagIDsToAdd]] isSubsetOfSet:[NSSet setWithArray: [self tagIDsToRemove]]];
+        BOOL invalidatesAdded = [[NSSet setWithArray: [otherTask tagIDStringsToAdd]] isSubsetOfSet:[NSSet setWithArray: [self tagIDStringsToRemove]]];
         
         // if we do both, we effectively null the effect of this operation
         return invalidatesRemoved && invalidatesAdded;
     }
     return NO;
+}
+
+- (NSArray *)dependenciesIn:(NSArray *)others
+{
+    NSMutableArray * dependencies = [NSMutableArray array];
+    for (INAPITask * other in others) {
+        if (other == self) {
+            continue;
+        }
+        if ([other isKindOfClass: [INSaveTagTask class]] && [[NSSet setWithArray:[self tagIDStringsToAdd]] containsObject:[[other model] ID]]) {
+            [dependencies addObject: other];
+        }
+    }
+    return dependencies;
 }
 
 
